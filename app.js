e921adab8b782f20ffe14f9e0298aa687aaacde5
ilijas19 <ilijagocic19@gmail.com>
2024-10-19 18:12:21 +0200

    added docs

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
-
+//security
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 //db
 const connectDb = require("./db/connectDb");
 //middlewares
@@ -23,15 +32,38 @@ const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const followerRouter = require("./routes/followerRoutes");
 const postRouter = require("./routes/postRoutes");
-//app
+const commentRouter = require("./routes/commentRoutes");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Social Network API</h1><a href='/api-docs'>Documentation</a>");
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload({ useTempFiles: true }));
+
+app.get("/api/v1", (req, res) => {
+  res.send("<h1>E-COMMERCE-API</h1>");
+});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/followers", followerRouter);
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/comment", commentRouter);
 
 app.use(notFound);
 app.use(errorHandler);
